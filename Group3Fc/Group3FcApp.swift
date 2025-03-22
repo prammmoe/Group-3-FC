3955b0f8909512225c013855a1c77bb44e7de9d2
filzarr <filzaramadhan2003@gmail.com>
2025-03-22 17:30:49 +0700

    initial commit

---
+++ Group3Fc/Group3FcApp.swift
@@ -0,0 +1,32 @@
+//
+//  Group3FcApp.swift
+//  Group3Fc
+//
+//  Created by Filza Rizki Ramadhan on 22/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Group3FcApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
